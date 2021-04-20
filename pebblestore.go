09b5435a8040c684ca31afd2b9f2cbceabec083d
M. J. Fromberger <michael.j.fromberger@gmail.com>
2021-04-20 09:55:33 -0400

    Implement blob.Store methods.

--- pebblestore.go
+++ pebblestore.go
@@ -17,64 +17,115 @@ package pebblestore
 
 import (
 	"context"
+	"io"
 
 	"github.com/cockroachdb/pebble"
 	"github.com/creachadair/ffs/blob"
 )
 
-// Store implements the blob.Store interface using a bbolt database.
+// Store implements the blob.Store interface using a Pebble database.
 type Store struct {
 	db *pebble.DB
+	c  io.Closer
 }
 
 // Opener constructs a filestore from an address comprising a path, for use
 // with the store package. If addr has the form name@path, the name is used as
 // the bucket label.
 func Opener(_ context.Context, addr string) (blob.Store, error) {
-	panic("ok")
+	return Open(addr, nil)
 }
 
-// Open creates a Store by opening the bbolt database specified by opts.
+// Open creates a Store by opening the Pebble database specified by opts.
 func Open(path string, opts *Options) (*Store, error) {
-	panic("ok")
+	db, err := pebble.Open(path, &pebble.Options{})
+	if err != nil {
+		return nil, err
+	}
+	return &Store{db: db, c: db}, nil
 }
 
-// Options provides options for opening a bbolt database.
-type Options struct {
-}
+// Options provides options for opening a Pebble database.
+type Options struct{}
+
+type nopCloser struct{}
+
+func (nopCloser) Close() error { return nil }
 
 // Close implements the io.Closer interface. It closes the underlying database
 // instance and reports its result.
-func (s *Store) Close() error { return s.db.Close() }
+func (s *Store) Close() error {
+	cerr := s.c.Close()
+	s.c = nopCloser{}
+	return cerr
+}
 
 // Get implements part of blob.Store.
 func (s *Store) Get(_ context.Context, key string) (data []byte, err error) {
-	panic("ok")
+	val, c, err := s.db.Get([]byte(key))
+	if err == pebble.ErrNotFound {
+		return nil, blob.ErrKeyNotFound
+	} else if err != nil {
+		return nil, err
+	}
+	data = make([]byte, len(val))
+	copy(data, val)
+	c.Close() // required; invalidates val
+	return data, nil
 }
 
-// Put implements part of blob.Store. A successful Put linearizes to the point
-// at which the rename of the write temporary succeeds; a Put that fails due to
-// an existing key linearizes to the point when the key path stat succeeds.
+// Put implements part of blob.Store.
 func (s *Store) Put(_ context.Context, opts blob.PutOptions) error {
-	panic("ok")
+	key := []byte(opts.Key)
+	if !opts.Replace {
+		_, c, err := s.db.Get(key)
+		if err == nil {
+			c.Close()
+			return blob.ErrKeyExists
+		}
+		// fall through
+	}
+	return s.db.Set(key, opts.Data, pebble.Sync)
 }
 
 // Size implements part of blob.Store.
-func (s *Store) Size(_ context.Context, key string) (size int64, err error) {
-	panic("ok")
+func (s *Store) Size(ctx context.Context, key string) (size int64, err error) {
+	data, err := s.Get(ctx, key)
+	if err != nil {
+		return 0, err
+	}
+	return int64(len(data)), nil
 }
 
 // Delete implements part of blob.Store.
-func (s *Store) Delete(_ context.Context, key string) error {
-	panic("ok")
+func (s *Store) Delete(ctx context.Context, key string) error {
+	if _, err := s.Get(ctx, key); err != nil {
+		return err
+	}
+	return s.db.Delete([]byte(key), pebble.Sync)
 }
 
 // List implements part of blob.Store.
 func (s *Store) List(_ context.Context, start string, f func(string) error) error {
-	panic("ok")
+	it := s.db.NewIter(&pebble.IterOptions{LowerBound: []byte(start)})
+	for it.First(); it.Valid(); it.Next() {
+		err := f(string(it.Key()))
+		if err == blob.ErrStopListing {
+			break
+		} else if err != nil {
+			it.Close()
+			return err
+		}
+	}
+	return it.Close()
 }
 
 // Len implements part of blob.Store.
 func (s *Store) Len(ctx context.Context) (int64, error) {
-	panic("ok")
+	it := s.db.NewIter(&pebble.IterOptions{LowerBound: []byte("")})
+	var count int64
+	for it.First(); it.Valid(); it.Next() {
+		count++
+	}
+	return count, it.Close()
 }
